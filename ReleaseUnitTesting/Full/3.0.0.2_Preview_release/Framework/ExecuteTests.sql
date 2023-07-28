76902153295651197738c4ba59649f0736167cf0
bajzatzoltan <44295559+bajzatzoltan@users.noreply.github.com>
2023-07-28 21:38:23 +0200

    Add execute scripts to 3.0.0.2 preview release

---
+++ ReleaseUnitTesting/Full/3.0.0.2_Preview_release/Framework/ExecuteTests.sql
@@ -0,0 +1,32 @@
+﻿USE UnitTesting;
+GO
+
+---------------------------------------------------
+--SET DEFAULT DATABASE:
+---------------------------------------------------
+DECLARE @databaseName VARCHAR(256) = 'TutorialsSqlServerUnitTestCiCd'; --Database name which was registered and want to test
+---------------------------------------------------
+--DELETE RESULTS OF UNIT TESTS:
+---------------------------------------------------
+TRUNCATE TABLE dbo.TestResults
+DBCC CHECKIDENT ('dbo.TestResults', RESEED, 1) WITH NO_INFOMSGS
+---------------------------------------------------
+--EXECUTE UNIT TESTS FROM REGISTERED DIRECTORY:
+---------------------------------------------------
+EXEC ExecuteTests_usp	@directoryName = 'CreateTestEnviroment', --Directory name and path in UnitTests directory which was registered and want to run all .sql files
+						@databaseName = @databaseName;
+EXEC ExecuteTests_usp	@directoryName = 'DependentObjects\Objects\Sales\Programmability', --Directory name and path in UnitTests directory which was registered and want to run all .sql files
+						@databaseName = @databaseName;
+--EXEC ExecuteTests_usp	@directoryName = 'DeleteTestEnviroment', --Directory name and path in UnitTests directory which was registered and want to run all .sql files
+--						@databaseName = @databaseName;
+GO
+-------------------------------------------------
+--PRESENTATION OF UNIT TESTS RESULTS:
+-------------------------------------------------
+BEGIN
+	SELECT * FROM TestResults WHERE TestResult = 'False' ORDER BY MethodName, TestNumber;
+	SELECT * FROM TestResults ORDER BY MethodName, TestNumber;
+END
+GO
+
+
